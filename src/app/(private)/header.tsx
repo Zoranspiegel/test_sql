8f406337326865f670f11375b8983f942210c95f
Juan José Rodriguez <zoranbow@gmail.com>
2024-01-27 03:34:51 -0500

    header error & loading styles

--- src/app/(private)/header.tsx
+++ src/app/(private)/header.tsx
@@ -5,8 +5,22 @@ import User from '../components/user';
 export default function Header (): JSX.Element {
   const { data, error, isLoading } = useSWR('/api/users/profile');
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>ERROR</div>;
+  if (isLoading) {
+    return (
+      <div className='flex w-full justify-between items-center rounded-lg bg-gray-800 py-4 px-6'>
+        <h1 className='text-2xl font-bold'>TestSQL</h1>
+        <div>Loading...</div>
+      </div>
+    );
+  };
+  if (error) {
+    return (
+      <div className='flex w-full justify-between items-center rounded-lg bg-gray-800 py-4 px-6'>
+        <h1 className='text-2xl font-bold'>TestSQL</h1>
+        <div>ERROR</div>
+      </div>
+    );
+  }
 
   return (
     <header
