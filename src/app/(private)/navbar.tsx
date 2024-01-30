250b90d63fe12fa45730868eed2780d6e16ae751
Juan José Rodriguez <zoranbow@gmail.com>
2024-01-29 21:23:52 -0500

    navbar active link styles

--- src/app/(private)/navbar.tsx
+++ src/app/(private)/navbar.tsx
@@ -1,25 +1,31 @@
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function NavBar (): JSX.Element {
+  const pathname = usePathname();
   return (
     <nav className='flex w-full justify-around items-center rounded-lg bg-gray-800 py-4 px-6'>
       <Link
         href='/feed'
+        className={pathname === '/feed' ? 'text-white' : ''}
       >
         Feed
       </Link>
       <Link
+        href='/profile'
+        className={pathname === '/profile' ? 'text-white' : ''}
+      >
+        Profile
+      </Link>
+      <Link
         href='/'
+        className={pathname === '/' ? 'text-white' : ''}
       >
         X
       </Link>
       <Link
         href='/'
-      >
-        X
-      </Link>
-      <Link
-        href='/'
+        className={pathname === '/' ? 'text-white' : ''}
       >
         X
       </Link>
